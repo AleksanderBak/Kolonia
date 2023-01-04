62a4e0032438567e41d1fb8a124761b8a1150cad
AleksanderBak <aleksanderbak9@gmail.com>
2023-01-04 23:02:36 +0100

    New item/edit item forms

--- kolonia/urls.py
+++ kolonia/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
+    path("pomieszczenia", views.rooms, name="rooms"),
+    path("pomieszczenia/new", views.roomsNew, name="rooms_new"),
+    path("pomieszczenia/<id>", views.roomsEdit, name="rooms_edit"),
 ]
--- kolonia/views.py
+++ kolonia/views.py
@@ -3,5 +3,29 @@ from .models import Pomieszczenia
 
 # Create your views here.
 def dashboard(request):
+    # output = Pomieszczenia.objects.raw("SELECT * FROM pomieszczenia")
+    return render(request, "dashboard.html")
+
+
+def rooms(request):
     output = Pomieszczenia.objects.raw("SELECT * FROM pomieszczenia")
-    return render(request, "dashboard.html", {"Pomieszczenia": output})
+    return render(request, "rooms.html", {"Pomieszczenia": output})
+
+
+def roomsNew(request):
+    if request.method == "POST":
+        name = request.POST["nazwa"]
+        desc = request.POST["opis"]
+        Pomieszczenia.objects.create(nazwa=name, opis=desc)
+    return render(request, "rooms_new.html")
+
+
+def roomsEdit(request, id):
+    if request.method == "POST":
+        name = request.POST["nazwa"]
+        desc = request.POST["opis"]
+        id = request.POST["id"]
+        Pomieszczenia.objects.filter(nr_pomieszczenia=id).update(nazwa=name, opis=desc)
+    query = "SELECT * FROM pomieszczenia WHERE nr_pomieszczenia={id}".format(id=id)
+    output = Pomieszczenia.objects.raw(query)[0]
+    return render(request, "rooms_edit.html", {"Pomieszczenie": output})
